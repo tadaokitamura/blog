67a3da8778728df3b92c8f166b4aca7532ee7b48
vagrant <vagrant@blog.local>
2017-05-23 13:40:29 +0900

    各ファイルの微調整

--- app/Http/routes.php
+++
@@ -1,8 +0,0 @@
-Route::get('/posts', 'PostsController@index')->name('posts.index');
-Route::get('/posts/create', 'PostsController@create')->name('posts.create');
-Route::get('/posts/{post}', 'PostsController@show')->name('posts.show');
-Route::post('/posts', 'PostsController@store')->name('posts.store');
-Route::get('/posts/{post}/edit', 'PostsController@edit')->name('posts.edit');
-Route::put('/posts/{post}', 'PostsController@update')->name('posts.update');
-Route::delete('/posts/{post}', 'PostsController@destroy')->name('posts.destroy');
-
--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-	<meta charset="UTF-8">
-	<title>一覧画面です</title>
-</head>
-<body>
-<h1>一覧画面です</h1>
-</body>
-</html>
